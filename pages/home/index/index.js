0f251a05cae7df0f5d01051608810a3088480820
ouzhibing <ouzhibing@galaxymx.com>
2019-04-16 16:05:44 +0800

    完成单聊的基本功能

--- pages/home/index/index.js
+++ pages/home/index/index.js
@@ -5,19 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    showAuth:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.checkoutAuth();
+  },
 
+  checkoutAuth:function(){
+    let that = this;
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userInfo']) {
+          that.setData({
+            showAuth: true
+          });
+        }
+      }
+    })
+  },
+
+  /**
+ * 监听用户点击授权按钮
+ */
+  getAuthUserInfo: function (data) {
+    this.setData({
+      showAuth: false
+    });
+    wx.setStorageSync('user',data.detail.userInfo);
   },
 
   navigateSingle:function(){
     wx.navigateTo({
       url: '/pages/wechat/single_chat/single_chat'
     })
-  }
+  },
+
 })
--- pages/wechat/single_chat/single_chat.js
+++ pages/wechat/single_chat/single_chat.js
@@ -11,6 +11,7 @@ const DATE = 4;
 const RED_PACKET = 5;
 const RECEIVED_RED_PACKET = 6;
 const TRANSFER_AMOUNT = 7;
+const RECEIVE_TRANSFER_AMOUNT = 8;
 
 const icon = 'http://image.kucaroom.com//tmp/wx0f587d7c97a68e2b.o6zAJs3oh85Zb1lJE8oWix57vny0.ATIzBaoptXWG8c3ea5b7fe584cf68b45959a9f934eee.png';
 
@@ -42,11 +43,17 @@ Page({
 
     textContent:'',
     messageType: IMG,
-    redPacketTitle:'',
-    selectUser:1
+    selectUser:0,
+    selectReceiveUser:1,
+    transferAmount:0,
+    getTransferAmountTo:0,
+    redPacketTitle:'恭喜发财，大吉大利'
   },
 
   onLoad: function (option) {
+    this.setMember();
+    this.setStorageData();
+
     wx.setNavigationBarTitle({ title: this.data.leftUser.nickname });
     wx.setNavigationBarColor({
       frontColor: '#000000',
@@ -58,8 +65,55 @@ Page({
     })
   },
 
-  getLeftNickname:function(e){
+  /**
+   * 清空缓存
+   */
+  clearChat:function(){
+    wx.removeStorageSync('single_member');
+    wx.removeStorageSync('single_chat');
+    this.setData({
+      rightUser: { nickname: '', avatar: '' },
+      hiddenRightInput: false,
+      leftUser: { nickname: '', avatar: '' },
+      hiddenLeftInput: false,
+      chatList:[]
+    });
+  },
 
+  /**
+   * 根据缓存加载数据
+   */
+  setStorageData:function(){
+    let data = wx.getStorageSync('single_chat');
+    console.log(data);
+    if(data != '' && data != undefined && data.length > 0){
+      this.setData({chatList:data})
+    }
+  },
+
+  /**
+   * 根据缓存信息设置右边用户的信息
+   */
+  setMember:function(){
+    let user = wx.getStorageSync('user');
+    let memberInfo = wx.getStorageSync('single_member');
+    let leftUser = memberInfo.left;
+    let rightUser = memberInfo.right;
+
+    if (rightUser != '' && rightUser != undefined){
+      this.setData({ rightUser: rightUser, hiddenRightInput: true });
+    }else{
+      if (user) {
+        let userInfo = this.data.rightUser;
+        userInfo.nickname = user.nickName;
+        userInfo.avatar = user.avatarUrl;
+        this.setData({ rightUser: userInfo, hiddenRightInput: true });
+      }
+    }
+
+    if (leftUser != '' && leftUser != undefined) {
+      this.setData({ leftUser: leftUser, hiddenLeftInput: true });
+    }
   },
 
   operate:function(){
@@ -70,6 +124,7 @@ Page({
  * 触摸屏幕后移动触发一些隐藏操作
  */
   hiddenOperate: function () {
+    console.log("test");
     this.setData({
       showOperate: false
     });
@@ -90,7 +145,6 @@ Page({
    * 获取上传的图片
    */
   uploadSuccess: function (uploadData) {
-    console.log("发送图片");
     let attachments = [];
     attachments.push(uploadData.detail.key)
     this.send(null,attachments);
@@ -101,7 +155,7 @@ Page({
    * 设置title
    */
   setTitle: function (id,cantChat){
-    wx.setNavigationBarTitle({ title: '邱慧怡' });
+    
   },
 
   /**
@@ -116,7 +170,7 @@ Page({
   },
 
   /**
-  * 选择头像
+  * 选择聊天成员头像
   */
   selectImage: function (e) {
     let type  = e.currentTarget.dataset.type;
@@ -140,11 +194,16 @@ Page({
           user.avatar = filePaths[0];
           this.setData({ rightUser: user })
         }
+
+        wx.setStorageSync('single_member', { 'left': this.data.leftUser, 'right': this.data.right });
       }
     })
 
   },
 
+  /**
+   * 获取聊天成员昵称
+   */
   getNickname: function (e){
     let value = e.detail.value;
     let type = e.currentTarget.dataset.type;
@@ -157,23 +216,28 @@ Page({
     } else {
       let user = this.data.rightUser;
       user.nickname = value;
-      this.setData({ rightUser: user })
+      this.setData({ rightUser: user });
     }
   },
 
+  /**
+   * 添加聊天成员输入框失去焦点
+   */
   loseCous:function(e){
     let type = e.currentTarget.dataset.type;
     let value = e.detail.value;
-
-    console.log(value)
-
     if (type == 0 && value != '') {
       this.setData({ hiddenLeftInput: true })
     } else if(value != '') {
       this.setData({ hiddenRightInput: true })
     }
+
+    wx.setStorageSync('single_member', {'left':this.data.leftUser,'right':this.data.right});
   },
 
+  /**
+   * 显示聊天成员昵称输入框
+   */
   showInputView:function(e){
     let type = e.currentTarget.dataset.type;
     if (type == 0) {
@@ -183,10 +247,9 @@ Page({
     }
   },
 
-  addText:function(){
-    
-  },
-
+  /**
+   * 隐藏添加功能面板
+   */
   closeCreateView:function(){
     this.setData({ showCreateView:false})
   },
@@ -219,7 +282,7 @@ Page({
         title: '请填写右边用户的昵称',
         icon: 'none'
       })
-      return false;
+     return false;
     }
 
     if (rightUser.nickname == '') {
@@ -255,7 +318,6 @@ Page({
    * 添加消息
    */
   pushMessage:function(e){
-    
     let user = this.data.leftUser;
     if(this.data.selectUser == 1){
       user = this.data.rightUser;
@@ -263,11 +325,10 @@ Page({
 
     let type = this.data.messageType;
     let chatData = this.data.chatList;
-
     let template = {
       data_type: this.data.selectUser,
       user: { nickname: '', avatar: '' },
-      message: { type: 1, content: '' }
+      message: { type: 1, content: '' ,attachment:''}
     };
 
     template.message.type = type;
@@ -283,9 +344,32 @@ Page({
         template.message.content = this.data.redPacketTitle;
         chatData.push(template);
         break;
+      case RECEIVED_RED_PACKET:
+        let receiveUser = this.data.selectReceiveUser;
+        template.message.content = this.data.redPacketTitle;
+        template.message.attachment = receiveUser==0?this.data.leftUser.nickname:this.data.rightUser.nickname;
+        chatData.push(template);
+        break;
+      case TRANSFER_AMOUNT:
+        let content = '';
+        if (this.data.getTransferAmountTo==0){
+          content = '转账给' + this.data.leftUser.nickname;
+        }else{
+          content = '转账给' + this.data.rightUser.nickname;
+        }
+        template.message.content = content;
+        template.message.attachment = this.data.transferAmount;
+        chatData.push(template);
+        break;
+      case RECEIVE_TRANSFER_AMOUNT:
+        template.message.content = '已收钱';
+        template.message.attachment = this.data.transferAmount;
+        chatData.push(template);
+        break;
     }
 
     this.setData({ chatList: chatData, showCreateView: false, showOperate: false});
+    wx.setStorageSync('single_chat', chatData);
     wx.showToast({
       title: '添加成功',
       icon:'none'
@@ -312,6 +396,11 @@ Page({
             message: { type: 1, content: '' }
           };
 
+          let user = this.data.leftUser;
+          if (this.data.selectUser == 1) {
+            user = this.data.rightUser;
+          }
+
           template.type = this.data.selectUser;
           template.message.type = IMG;
           template.message.content = item;
@@ -321,6 +410,7 @@ Page({
         })
 
         this.setData({ chatList: chatData, showCreateView: false, showOperate:false});
+        wx.setStorageSync('single_chat', chatData);
         setTimeout(res => {
           wx.pageScrollTo({
             scrollTop: this.data.scrollTop += 1000
@@ -334,8 +424,30 @@ Page({
    * 选着发送信息的聊天成员
    */
   selectChatUser:function(e){
-    console.log("select");
     let type = e.currentTarget.dataset.type;
     this.setData({ selectUser: type })
+  },
+
+  /**
+   * 选择收红包的人
+   */
+  selectReceiveMan:function(e){
+    let type = e.currentTarget.dataset.type;
+    this.setData({ selectReceiveUser: type })
+  },
+
+  /**
+   * 选择转账给某人
+   */
+  selectTransferAmountTo:function(e){
+    let type = e.currentTarget.dataset.type;
+    this.setData({ getTransferAmountTo: type })
+  },
+
+  /**
+   * 获取转账金额
+   */
+  getTransferAmount:function(e){
+    this.setData({ transferAmount:e.detail.value}) 
   }
 })
